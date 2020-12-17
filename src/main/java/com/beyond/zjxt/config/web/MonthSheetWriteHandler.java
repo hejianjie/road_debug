e262983e4c6f54993c2a152247e11a87e3e755e1
wdz <969617731@qq.com>
2020-12-17 14:56:42 +0800

    excel模板(细目表图片未解决)

--- src/main/java/com/beyond/zjxt/config/web/MonthSheetWriteHandler.java
+++ src/main/java/com/beyond/zjxt/config/web/MonthSheetWriteHandler.java
@@ -20,17 +20,7 @@ public class MonthSheetWriteHandler implements SheetWriteHandler {
     @Override
     public void afterSheetCreate(WriteWorkbookHolder writeWorkbookHolder, WriteSheetHolder writeSheetHolder) {
         Workbook workbook = writeWorkbookHolder.getWorkbook();
-        Sheet sheet = workbook.getSheetAt(0);
-        Row row1 = sheet.createRow(0);
-        row1.setHeight((short) 500);
-        Cell cell = row1.createCell(0);
-        //设置单元格内容
-        cell.setCellValue("附件2");
-        //设置标题
-        Row row2 = sheet.createRow(1);
-        row2.setHeight((short) 800);
-        Cell cell1 = row2.createCell(0);
-        cell1.setCellValue("2019年大连市干线公路小修工程完成量汇总表(3月份）");
+        // 样式
         CellStyle cellStyle = workbook.createCellStyle();
         cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
         cellStyle.setAlignment(HorizontalAlignment.CENTER);
@@ -38,12 +28,49 @@ public class MonthSheetWriteHandler implements SheetWriteHandler {
         font.setBold(true);
         font.setFontHeight((short) 400);
         cellStyle.setFont(font);
-        cell1.setCellStyle(cellStyle);
-        sheet.addMergedRegionUnsafe(new CellRangeAddress(1, 1, 0, 17));
-        //设置填表日期,填报人,联系方式
-        Row row3 = sheet.createRow(2);
-        row3.setHeight((short) 500);
-        row3.createCell(1).setCellValue("填报单位（盖章）：大连市甘井子区公路管理段");
-        row3.createCell(10).setCellValue("填报日期：2019年5月7日");
+        Font font1 = workbook.createFont();
+        font1.setBold(true);
+        font1.setFontHeight((short) 300);
+        CellStyle cellStyle1 = workbook.createCellStyle();
+        cellStyle1.setFont(font1);
+        if (writeSheetHolder.getSheetNo() == 0) {
+            // 小修汇总表
+            Sheet sheet = workbook.getSheetAt(0);
+            //设置标题
+            Row row1 = sheet.createRow(0);
+            row1.setHeight((short) 800);
+            Cell cell1 = row1.createCell(0);
+            cell1.setCellValue("2019年大连市干线公路小修工程完成量汇总表(3月份）");
+            cell1.setCellStyle(cellStyle);
+            sheet.addMergedRegionUnsafe(new CellRangeAddress(0, 0, 0, 12));
+            //设置填表日期,填报人,联系方式
+            Row row2 = sheet.createRow(1);
+            row2.setHeight((short) 500);
+            Cell row2Cell1 = row2.createCell(0);
+            row2Cell1.setCellValue("填报单位（盖章）：大连市甘井子区公路管理段");
+            Cell row2Cell2 = row2.createCell(9);
+            row2Cell2.setCellValue("填报日期：2019年5月7日");
+            row2Cell1.setCellStyle(cellStyle1);
+            row2Cell2.setCellStyle(cellStyle1);
+        }
+        if (writeSheetHolder.getSheetNo() == 1) {
+            // 小修明细表
+            Sheet sheet1 = workbook.getSheetAt(1);
+            //设置标题
+            Row sheet1Row = sheet1.createRow(0);
+            sheet1Row.setHeight((short) 800);
+            Cell sheet1RowCell = sheet1Row.createCell(0);
+            sheet1RowCell.setCellValue("2019年甘井子区干线公路小修工程完成情况明细表(3月份）");
+            sheet1RowCell.setCellStyle(cellStyle);
+            sheet1.addMergedRegionUnsafe(new CellRangeAddress(0, 0, 0, 20));
+            //设置填表日期,填报人,联系方式
+            Row sheet1Row1 = sheet1.createRow(1);
+            Cell sheet1Row1Cell = sheet1Row1.createCell(0);
+            sheet1Row1Cell.setCellValue("填报单位（盖章）：大连市甘井子区公路管理段");
+            Cell sheet1Row1Cell1 = sheet1Row1.createCell(15);
+            sheet1Row1Cell1.setCellValue("填报日期：2019年5月7日");
+            sheet1Row1Cell.setCellStyle(cellStyle1);
+            sheet1Row1Cell1.setCellStyle(cellStyle1);
+        }
     }
 }
--- src/main/java/com/beyond/zjxt/modular/road/entity/SummaryOfMinorRepairsHead.java
+++ src/main/java/com/beyond/zjxt/modular/road/entity/SummaryOfMinorRepairsHead.java
@@ -1,6 +1,7 @@
 package com.beyond.zjxt.modular.road.entity;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.alibaba.excel.annotation.write.style.ContentRowHeight;
 import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import lombok.Data;
@@ -15,45 +16,58 @@ import lombok.experimental.Accessors;
 @Data
 @NoArgsConstructor
 @Accessors(chain = true)
-@ContentRowHeight(45)
-@HeadRowHeight(50)
+@ContentRowHeight(35)
+@HeadRowHeight(40)
 public class SummaryOfMinorRepairsHead {
+    @ColumnWidth(10)
     @ExcelProperty(value = {"序号"}, index = 0)
     private Long orderNum;
 
+    @ColumnWidth(20)
     @ExcelProperty(value = {"项目名称"}, index = 1)
     private String projectName;
 
+    @ColumnWidth(20)
     @ExcelProperty(value = {"细目名称"}, index = 2)
     private String smallProjectName;
 
+    @ColumnWidth(15)
     @ExcelProperty(value = {"建设性质"}, index = 3)
     private String projectType;
 
+    @ColumnWidth(15)
     @ExcelProperty(value = {"单位"}, index = 4)
     private String unit;
 
-    @ExcelProperty(value = {"单价（元）"}, index = 5)
+    @ColumnWidth(15)
+    @ExcelProperty(value = {"单价(元)"}, index = 5)
     private String unitPrice;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"计划","作业工作量"}, index = 6)
     private String workAmount;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"计划","作业频率"}, index = 7)
     private String operationFrequency;
 
-    @ExcelProperty(value = {"计划","金额（万元）"}, index = 8)
+    @ColumnWidth(10)
+    @ExcelProperty(value = {"计划","金额(万元)"}, index = 8)
     private String price;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"完成","作业工程量"}, index = 9)
     private String finWorkAmount;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"完成","作业频率"}, index = 10)
     private String finOperationFrequency;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"完成","金额(万元)"}, index = 11)
     private String finPrice;
 
+    @ColumnWidth(10)
     @ExcelProperty(value = {"备注"}, index = 12)
     private String note;
 }
